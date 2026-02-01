f3935865eb02680f2b2339e6db16de920ea16e41
Jonathan Matarazzi <git@jonathanm.nl>
2026-02-01 11:14:43 +0100

    assignment 2-7

--- src/centerofmass/ghci/HCenterOfMass12.hs
+++ src/centerofmass/ghci/HCenterOfMass12.hs
@@ -22,7 +22,7 @@ thresholdIm :: (Num a, Ord a)
   => [[a]] -- list of list of values
   -> a     -- threshold value
   -> [[a]] -- list of list of values with updated value
-thresholdIm img thr = 
+thresholdIm img thr =
   [ [ if pixel >= thr then 1 else 0 | pixel <- row ] | row <- img ]
 -- command used to test in ghci: wf lighthouseBW (thresholdIm image 128)
 
@@ -33,8 +33,43 @@ lightHouseBW = "../images/lightHouseBW.pgm"
 -- Assignment 2, Center of mass of rows and picture
 -----------------------------------------------------------------------------------------
 
+comRows :: [[Int]] -> Int
+comRows image = y-1
+  where
+    y = div (sum cmy) (sum my)
+    cmy = zipWith (*) [1..] my
+    my = map sum image
+
+com :: [[Int]] -> (Int, Int)
+com image = (x, y)
+  where
+    y = comRows image
+    x = comRows (transpose image)
+
+imageWithCom :: [[Int]] -> Int -> [[Int]]
+imageWithCom image = changePixelInImage image y x
+  where
+    (x, y) = com image
 
 -----------------------------------------------------------------------------------------
 -- Assignment 3 Center of mass of parts of the image, with and without borders
 -----------------------------------------------------------------------------------------
 
+comParts :: [[Int]] -> [[Int]]
+comParts image = unblocks2D height processedBlocks
+  where
+    height = length image
+    blocks = blocks2D 8 image
+    processedBlocks = map processBlock blocks
+    processBlock block =  if sum (map sum block) == 0
+                          then changePixelInImage block 4 4 2
+                          else imageWithCom block 2
+
+    
+comPartsWB :: [[Int]] -> [[Int]]
+comPartsWB image = unblocks2D newHeight blocksWB
+  where
+    height = length image
+    newHeight = height + (div height 8) * 2
+    blocks = blocks2D 8 (comParts image)
+    blocksWB = addBorders 2 blocks
